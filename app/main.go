b4627512e45ae0230900d0dc39d5a7e3f10c37da
kdoebr21 <kodeberic@yahoo.co.jp>
2019-07-30 00:39:37 +0900

    feat: jsonレスポンスのサンプル実装

--- app/main.go
+++ app/main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
+func getHelloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world!!")
+}
+
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Cat name is: %s\nand his type is: %s\n", catName, catType))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name":    catName,
+			"catType": catType,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "You need to lets us know if you want json or string data",
+	})
+
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, world!!")
-	})
+	e.GET("/", getHelloWorld)
+	e.GET("/cats/:data", getCats)
 	e.Logger.Fatal(e.Start(":8080"))
 }
